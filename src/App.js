df96e2774cf91befe89bb51e3db837f6e8e63b64
Tolulomo Fateye <tolulomofateye@gmail.com>
2019-12-16 12:41:58 -0500

    Completed App routes boiler-plate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>
-          Let's create a view that displays user data grouped by the project
-          each user is working on.
-        </h3>
-        <p>
-          Start by looking at <code>src/mock_data/users.js</code> and{" "}
-          <code>src/mock_data/projects.js</code>.
-        </p>
-      </header>
-    </div>
-  );
+const Header = () => <div>Header</div>
+const Welcome = () => <div>Welcome</div>
+const Project = () => <div>Project</div>
+const AddProject = () => <div>Add Project</div>
+const AddUser = () => <div>Add User Page</div>
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/project" component={Project} />
+          <Route path="/project/new" component={AddProject} />
+          <Route path="/project/add_user" component={AddUser} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
+// import React from "react";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h3>
+//           Let's create a view that displays user data grouped by the project
+//           each user is working on.
+//         </h3>
+//         <p>
+//           Start by looking at <code>src/mock_data/users.js</code> and{" "}
+//           <code>src/mock_data/projects.js</code>.
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,30 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { render } from "@testing-library/react";
+import renderer from 'react-test-renderer';
 import App from "./App";
 
-test("renders explanation text and starting directions", () => {
-  const { getByText } = render(<App />);
-  const explanationTextHeader = getByText(
-    /Let's create a view that displays user data grouped by the project each user is working on/i
-  );
-  const directionsElement = getByText(/Start by looking at /i);
-  expect(explanationTextHeader).toBeInTheDocument();
-  expect(directionsElement).toBeInTheDocument();
+// test("renders explanation text and starting directions", () => {
+//   const { getByText } = render(<App />);
+//   const explanationTextHeader = getByText(
+//     /Let's create a view that displays user data grouped by the project each user is working on/i
+//   );
+//   const directionsElement = getByText(/Start by looking at /i);
+//   expect(explanationTextHeader).toBeInTheDocument();
+//   expect(directionsElement).toBeInTheDocument();
+// });
+
+it('renders wihout crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('App', () => {
+  test('snapshot renders', () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
